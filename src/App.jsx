aaed766aed28fe87ffcce76716b3d2429163baaa
Pete Laptop <pmugasa78@gmail.com>
2023-05-21 00:16:17 +0200

    implemented the hover effect on the images to show title and delete button

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,30 @@ import ModalDelete from "./components/ModalDelete";
 function App() {
   const [showOverlay, setShowOverlay] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [deleteModal, setDeleteModal] = useState(true);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const images = [
+    {
+      label: "a truck parked in a field with mountains in the background",
+      photoUrl: "https://source.unsplash.com/2ICHBMyhbCs",
+    },
+    {
+      label: "Thinking in the sky",
+      photoUrl: "https://source.unsplash.com/tZH8xyGGMHE",
+    },
+    {
+      label:
+        "The silhouette of a man walks past a store front that reads, 'Hot Corned Beef'",
+      photoUrl: "https://source.unsplash.com/oYyO-NS7oxc",
+    },
+    {
+      label: "The programmer doing his thing",
+      photoUrl: "https://source.unsplash.com/IGvHWIZxQ8Q",
+    },
+    {
+      label: "Skateboarding in the rainy day",
+      photoUrl: "https://source.unsplash.com/54IuUIToijY",
+    },
+  ];
   function handleHover() {
     setShowOverlay(!showOverlay);
   }
@@ -20,83 +43,40 @@ function App() {
         <Navbar showModal={showModal} setShowModal={setShowModal} />
 
         <div className="w-full mt-12 ">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <div className="grid gap-4">
-              <div
-                onMouseEnter={handleHover}
-                onMouseLeave={handleHover}
-                className="h-auto max-w-full rounded-lg bg-[url('/adrien-stachowiak-sUBlt91FXTw-unsplash.jpg')] bg-cover relative flex"
-              >
-                <img
-                  className="h-auto max-w-full rounded-lg invisible"
-                  src="/Awesome Get off My Computer Wallpapers - WallpaperAccess.png"
-                />
-
-                {showOverlay && (
-                  <>
-                    <div className="absolute w-full h-full bg-gray-900 opacity-60 top-0 left-0"></div>
-                    <button
-                      onClick={() => setDeleteModal(true)}
-                      className=" absolute top-3 right-3 border border-red-500 text-sm px-4 rounded-lg font-medium text-red-500 hover:bg-red-500 hover:text-white font-secondary-font"
+          <div className="columns-2 sm:columns-3 gap-4">
+            {images.map((image) => {
+              return (
+                <>
+                  <div className="mb-4" key={image.photoUrl}>
+                    <div
+                      className={
+                        `bg-[url('${image.photoUrl}')] ` +
+                        "h-auto max-w-full rounded-lg  bg-cover relative flex group"
+                      }
                     >
-                      delete
-                    </button>
-                    <h3 className="absolute bottom-2 left-3 font-primary-font text-white font-bold text-lg">
-                      Adrien Stachwoiak image title or something
-                    </h3>
-                  </>
-                )}
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="/Awesome Get off My Computer Wallpapers - WallpaperAccess.png"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="/Escape [1680x1050].jpeg"
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="/Ghost Rider[1920x1080].jpeg"
-                />
-              </div>
-              <div>
-                <img className="h-auto max-w-full rounded-lg" src="/man.jpg" />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="/plants.jpg"
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="/Sunset 1080P, 2K, 4K, 5K HD wallpapers free download.jpeg"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="/Wallpaper 4K Para Pc Windows 10 Ideas.jpeg"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="/wallpapersden.com_76045-3840x2160.jpg"
-                />
-              </div>
-            </div>
+                      <img
+                        className="h-auto max-w-full rounded-lg "
+                        src={image.photoUrl}
+                      />
+
+                      <div className="hidden group-hover:block">
+                        <div className="absolute w-full h-full bg-gray-900 opacity-60 top-0 left-0"></div>
+                        <button
+                          onClick={() => setDeleteModal(true)}
+                          className=" absolute top-3 right-3 border border-red-500 text-sm px-4 rounded-lg font-medium text-red-500 hover:bg-red-500 hover:text-white font-secondary-font"
+                        >
+                          delete
+                        </button>
+                        <h3 className="absolute bottom-2 left-3 font-primary-font text-white font-medium text-sm">
+                          {image.label}
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                  ;
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
