7e77cbf565de132a7ee5dc23fbc7ef49cfe4e42f
Pete Laptop <pmugasa78@gmail.com>
2023-05-21 13:47:52 +0200

    fixed the bug with the overlay when hovering state

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,24 +1,21 @@
 function ImageList({ image, deleteImage }) {
   return (
     <>
-      <div className="mb-4" key={image.id}>
-        <div
-          className={
-            `bg-[url('${image.photoUrl}')] ` +
-            "h-auto max-w-full rounded-lg  bg-cover relative flex group"
-          }
-        >
-          <img className="h-auto max-w-full rounded-lg " src={image.photoUrl} />
+      <div className="mb-4 h-auto max-w-full" key={image.id}>
+        <div className="h-auto  rounded-lg  bg-cover  flex group  relative">
+          <img
+            className="h-auto w-full rounded-lg object-cover "
+            src={image.photoUrl}
+          />
 
-          <div className="hidden group-hover:block">
-            <div className="absolute w-full h-full bg-[rgba(0,0,0,0.5)] top-0 left-0 rounded-md"></div>
+          <div className="hidden group-hover:block  absolute z-1 bg-[rgba(0,0,0,0.4)] h-full w-full p-4 flex-col space-y-10">
             <button
               onClick={() => deleteImage(image.id)}
-              className=" absolute top-3 right-3 border border-red-500 text-sm px-4 rounded-lg font-medium text-red-500 hover:bg-red-500 hover:text-white font-secondary-font"
+              className=" border border-red-500 text-sm px-4 rounded-lg  font-medium text-red-500  hover:border-2"
             >
               delete
             </button>
-            <p className="absolute white-space-pre-line w-96  bottom-2 left-3 font-primary-font text-white font-medium text-sm">
+            <p className="white-space-pre-line  font-primary-font text-white font-bold text-md">
               {image.label}
             </p>
           </div>
